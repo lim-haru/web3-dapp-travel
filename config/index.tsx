dde73a17e9883b62ac9382f9e5aeb375c3ff5879
lim-haru <123555071+lim-haru@users.noreply.github.com>
2024-08-09 15:24:52 +0200

    Configuration WalletConnect

---
+++ config/index.tsx
@@ -0,0 +1,41 @@
+import { http, createConfig, cookieStorage, createStorage } from "wagmi"
+import { mainnet, sepolia } from "wagmi/chains"
+import { coinbaseWallet, injected, walletConnect } from "wagmi/connectors"
+import { authConnector } from "@web3modal/wagmi"
+
+export const projectId = process.env.NEXT_PUBLIC_WC_PROJECT_ID
+
+if (!projectId) throw new Error("Project ID is not defined")
+
+export const metadata = {
+  name: "Appkit",
+  description: "AppKit Example",
+  url: "https://web3modal.com", // origin must match your domain & subdomain
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+}
+
+const chains = [mainnet, sepolia] as const
+export const config = createConfig({
+  chains,
+  connectors: [
+    injected(),
+    coinbaseWallet({ appName: metadata.name, appLogoUrl: metadata.icons[0] }),
+    walletConnect({ projectId, metadata, showQrModal: false }),
+    authConnector({
+      chains,
+      options: { projectId },
+      email: true,
+      socials: ["google", "x", "discord"],
+      showWallets: true,
+      walletFeatures: true,
+    }),
+  ],
+  transports: {
+    [mainnet.id]: http(),
+    [sepolia.id]: http(),
+  },
+  ssr: true,
+  storage: createStorage({
+    storage: cookieStorage,
+  }),
+})
